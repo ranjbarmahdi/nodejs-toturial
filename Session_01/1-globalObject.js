4fa1eb32b131f625a58f8087b645d91ae93c0f63
ranjbarmahdi <ranmahdi66@gmail.com>
2024-07-22 09:57:09 -0700

    a

---
+++ Session_01/1-globalObject.js
@@ -0,0 +1,43 @@
+
+// console.log(global);
+
+
+// console.log(Object.getOwnPropertyNames(global));
+
+// for (const prop in global) {
+//     console.log(prop);
+// }
+
+
+
+// ============================================== process
+
+// console.log(process.version);   // Node Version
+
+// console.log(process.versions);  // Verion of node and other thing
+
+// console.log(process.platform);  // Platform we use like win32
+
+// console.log(process.arch);  // x64
+
+// console.log(process.exit());  // exit
+
+// console.log(process.pid);  // process id
+
+// console.log(process.cwd());  // current working directory (where terminal is open)
+
+// console.log(process.argv);  //
+
+// console.log(__dirname); // what things run
+
+// console.log(__filename); // get __dirname+filename
+
+
+const x = {
+  "name": "John Doe",
+  "age": 30,
+  "email": "johndoe@example.com",
+
+}
+
+console.table(x)
